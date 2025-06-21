2ded46fdba46b77d079978416ba59a2c0c26a0ae
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-06-21 10:52:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
@@ -0,0 +1,62 @@
+// recursion
+
+// class Solution {
+//     int countWays(int n) {
+//         // your code here
+//         if(n==1){
+//             return 1;
+//         }
+        
+//         return countWays(n-1)+ countWays(n-2);
+//     }
+// }
+
+// Memoization
+
+
+class Solution {
+    int countWays(int n) {
+        // your code here
+       
+        
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        
+        return solve(n,dp);
+        
+      
+    }
+    
+    private int solve(int n, int dp[]){
+        
+         if(n==0){
+            return 1;
+        }
+         if(n==1){
+            return 1;
+        }
+        
+        if(dp[n] != -1) return dp[n];
+       return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    }
+}
+
+
+// Tabulation
+
+// class Solution {
+//     int countWays(int n) {
+//         // your code here
+//         if(n==1){
+//             return 1;
+//         }
+        
+//         int [] dp = new int[n+1];
+        
+//         dp[1] = 1;
+        
+        
+//         return countWays(n-1)+ countWays(n-2);
+//     }
+// }
+
