a03b6ae314cf7b49c5729b17f3500cde3df44a2f
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-06-21 12:09:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
@@ -14,49 +14,55 @@
 // Memoization
 
 
-class Solution {
-    int countWays(int n) {
-        // your code here
+// class Solution {
+//     int countWays(int n) {
+//         // your code here
        
         
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
         
-        return solve(n,dp);
+//         return solve(n,dp);
         
       
-    }
+//     }
     
-    private int solve(int n, int dp[]){
+//     private int solve(int n, int dp[]){
         
-         if(n==0){
-            return 1;
-        }
-         if(n==1){
-            return 1;
-        }
+//          if(n==0){
+//             return 1;
+//         }
+//          if(n==1){
+//             return 1;
+//         }
         
-        if(dp[n] != -1) return dp[n];
-       return dp[n] = solve(n-1, dp) + solve(n-2, dp);
-    }
-}
+//         if(dp[n] != -1) return dp[n];
+//       return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+//     }
+// }
 
 
 // Tabulation
 
-// class Solution {
-//     int countWays(int n) {
-//         // your code here
-//         if(n==1){
-//             return 1;
-//         }
+class Solution {
+    int countWays(int n) {
+        // your code here
+        if(n==1){
+            return 1;
+        }
         
-//         int [] dp = new int[n+1];
+        int [] dp = new int[n+1];
         
-//         dp[1] = 1;
+        dp[0] =1;
+        
+        dp[1] = 1;
         
         
-//         return countWays(n-1)+ countWays(n-2);
-//     }
-// }
+       for(int i=2; i<=n; i++){
+           dp[i] = dp[i-1]+dp[i-2]; 
+       }
+       
+       return dp[n];
+    }
+}
 
