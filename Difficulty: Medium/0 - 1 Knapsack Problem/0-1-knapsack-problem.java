4c234d394cfb784459bae3e01dfb05f2756e6d46
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-06-22 15:32:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -25,6 +25,43 @@
 //     }
 // }
 
+// class Solution {
+//     static int knapsack(int W, int val[], int wt[]) {
+//         // code here
+//         int n = val.length;
+        
+//         int [][]dp = new int[n+1][W+1];
+        
+//         for(int i=0; i<=n; i++){
+//         Arrays.fill(dp[i],-1);
+//         }
+        
+//         return solve(dp, val, wt, W, n);
+//     }
+    
+//     public static int solve(int [][]dp, int[] val, int [] wt, int W, int n){
+        
+//         if(W==0 || n==0){
+//             return 0;
+//         }
+        
+//         if(dp[n][W]!=-1){
+//             return dp[n][W];
+//         }
+//         if(wt[n-1] <=W){// valid
+        
+//         int include = val[n-1]+ solve(dp, val, wt, W-wt[n-1],n-1);
+//         int exclude = solve(dp, val, wt,W,n-1);
+        
+//         return dp[n][W] = Math.max(include, exclude);
+            
+//         }else{ // invalid
+//             return dp[n][W] = solve(dp, val, wt,W,n-1);
+//         }
+//     }
+// }
+
+
 class Solution {
     static int knapsack(int W, int val[], int wt[]) {
         // code here
@@ -32,33 +69,34 @@ class Solution {
         
         int [][]dp = new int[n+1][W+1];
         
-        for(int i=0; i<=n; i++){
-        Arrays.fill(dp[i],-1);
-        }
-        
-        return solve(dp, val, wt, W, n);
-    }
-    
-    public static int solve(int [][]dp, int[] val, int [] wt, int W, int n){
-        
         if(W==0 || n==0){
             return 0;
         }
         
-        if(dp[n][W]!=-1){
-            return dp[n][W];
+        for(int i=0; i<=n; i++){
+            dp[i][0] =0;
         }
-        if(wt[n-1] <=W){// valid
+        for(int j=0; j<=W; j++){
+            dp[0][j] =0;
+        }
         
-        int include = val[n-1]+ solve(dp, val, wt, W-wt[n-1],n-1);
-        int exclude = solve(dp, val, wt,W,n-1);
-        
-        return dp[n][W] = Math.max(include, exclude);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=W; j++ ){
+                 if(wt[i-1] <= j){// valid
+                int include = val[i-1] + dp[i-1][j-wt[i-1]];
+                int exclude = dp[i-1][j];
+                 dp[i][j] = Math.max(include, exclude);
+                 }else{ // invalid
+                 dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
             
-        }else{ // invalid
-            return dp[n][W] = solve(dp, val, wt,W,n-1);
-        }
+            
+            return dp[n][W];
+       
     }
 }
+
 
 
