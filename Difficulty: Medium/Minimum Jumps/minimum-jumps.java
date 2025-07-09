9d13f4ae29e2bdd20a7afbfaa0e763abf96eb78e
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-07-09 22:07:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.java
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -23,31 +23,59 @@
 // }
 
 
+// class Solution {
+//     static int minJumps(int[] arr) {
+//         int n = arr.length;
+
+//         if (n <= 1) return 0;
+//         if (arr[0] == 0) return -1;
+
+//         int jumps = 1;
+//         int maxReach = arr[0];
+//         int steps = arr[0];
+
+//         for (int i = 1; i < n; i++) {
+//             if (i == n - 1) return jumps;
+
+//             maxReach = Math.max(maxReach, i + arr[i]);
+//             steps--;
+
+//             if (steps == 0) {
+//                 jumps++;
+//                 if (i >= maxReach) return -1;
+//                 steps = maxReach - i;
+//             }
+//         }
+
+//         return -1;
+//     }
+
+// }
+
+
 class Solution {
     static int minJumps(int[] arr) {
         int n = arr.length;
 
-        if (n <= 1) return 0;
-        if (arr[0] == 0) return -1;
-
-        int jumps = 1;
-        int maxReach = arr[0];
-        int steps = arr[0];
-
-        for (int i = 1; i < n; i++) {
-            if (i == n - 1) return jumps;
-
-            maxReach = Math.max(maxReach, i + arr[i]);
-            steps--;
-
-            if (steps == 0) {
-                jumps++;
-                if (i >= maxReach) return -1;
-                steps = maxReach - i;
+        int dp[]  = new int [n];
+        Arrays.fill(dp,-1);
+        
+        dp[n-1] =0;
+        for(int i=n-2; i>=0; i--){
+            int steps = arr[i];
+            int ans = Integer.MAX_VALUE;
+            for(int j=i+1; j<=i+steps && j<n; j++){
+                if(dp[j] != -1){
+                    ans = Math.min(ans, dp[j]+1);
+                }
+            }
+            
+            if(ans != Integer.MAX_VALUE){
+                dp[i] = ans;
             }
         }
-
-        return -1;
+        
+        return dp[0]; 
     }
 
 }
