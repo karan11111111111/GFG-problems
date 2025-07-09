4657ca84bc44961eb7f86049d3b8ea349be17079
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-07-09 17:05:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nth Catalan Number/nth-catalan-number.java
@@ -0,0 +1,45 @@
+
+// class Solution {
+//     public static int findCatalan(int n) {
+//         // code here
+//         if(n==0 || n==1){
+//             return 1;
+//         }
+//         int ans =0;
+//         for(int i=0; i<n; i++){
+//              ans += findCatalan(i) * findCatalan(n-i-1);
+//         }
+        
+//         return ans;
+//     }
+// }
+
+class Solution {
+    public static int findCatalan(int n) {
+        // code here
+        
+        int dp[] = new int [n+1];
+        Arrays.fill(dp,-1);
+        
+        return solve(dp, n);
+        
+    }
+    
+    private static int solve(int[] dp , int n){
+        
+        if(n==0 || n==1){
+            return 1;
+        }
+        
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        
+        int ans =0;
+        for(int i=0; i<n; i++){
+             ans += solve(dp, i) * solve(dp,n-i-1);
+        }
+        dp[n] = ans;
+        return dp[n];
+    }
+}
