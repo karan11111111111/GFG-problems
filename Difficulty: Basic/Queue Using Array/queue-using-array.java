013664b77f06a401028018e18a5873d7b6e5d15f
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-07-26 16:59:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Queue Using Array/queue-using-array.java
+++ Difficulty: Basic/Queue Using Array/queue-using-array.java
@@ -1,29 +1,64 @@
-class MyQueue {
+// class MyQueue {
 
-    int front, rear;
-    int arr[] = new int[100005];
+//     int front, rear;
+//     int arr[] = new int[100005];
+
+//     MyQueue() {
+//         front = 0;
+//         rear = 0;
+//     }
+
+//     // Function to push an element x in a queue.
+//     void push(int x) {
+//         // Your code here
+//         arr[rear++] =x;
+        
+//     }
+
+//     // Function to pop an element from queue and return that element.
+//     int pop() {
+        
+//         // Your code here
+//         if(front == rear){
+//             return -1;
+//         }
+        
+//         return arr[front++];
+        
+//     }
+// }
+
+class MyQueue {
+    int front, rear, size;
+    int capacity = 100005;
+    int[] arr = new int[capacity];
 
     MyQueue() {
         front = 0;
         rear = 0;
+        size = 0;
     }
 
     // Function to push an element x in a queue.
     void push(int x) {
-        // Your code here
-        arr[rear++] =x;
-        
+        if (size == capacity) {
+            // Queue is full
+            return;
+        }
+        arr[rear] = x;
+        rear = (rear + 1) % capacity;
+        size++;
     }
 
     // Function to pop an element from queue and return that element.
     int pop() {
-        
-        // Your code here
-        if(front == rear){
+        if (size == 0) {
+            // Queue is empty
             return -1;
         }
-        
-        return arr[front++];
-        
+        int result = arr[front];
+        front = (front + 1) % capacity;
+        size--;
+        return result;
     }
 }
