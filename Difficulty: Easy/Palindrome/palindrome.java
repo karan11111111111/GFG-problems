8f27544b049767eba21871c1ed2fbee5ae85b33d
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-08-13 14:19:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome/palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int n) {
+        // code here
+        String str = String.valueOf(n);
+        String rev = new StringBuilder(str).reverse().toString();
+        if(str.equals(rev)){
+            return true;
+        }
+       return false;
+    }
+}
