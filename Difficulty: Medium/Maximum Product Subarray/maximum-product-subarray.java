d3357a58cdbf2559fce8b28db7689c771dc1c5b4
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-08-13 12:39:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
@@ -16,29 +16,68 @@
 //     }
 // }
 
-class Solution {
-    int maxProduct(int[] arr) {
-        int n = arr.length;
-        int maxProd = arr[0];
-        int minProd = arr[0];
-        int ans = arr[0];
+// class Solution {
+//     int maxProduct(int[] arr) {
+//         int n = arr.length;
+//         int maxProd = arr[0];
+//         int minProd = arr[0];
+//         int ans = arr[0];
 
-        for (int i = 1; i < n; i++) {
+//         for (int i = 1; i < n; i++) {
           
-            if (arr[i] < 0) {
-                int temp = maxProd;
-                maxProd = minProd;
-                minProd = temp;
-            }
+//             if (arr[i] < 0) {
+//                 int temp = maxProd;
+//                 maxProd = minProd;
+//                 minProd = temp;
+//             }
 
            
-            maxProd = Math.max(arr[i], maxProd * arr[i]);
-            minProd = Math.min(arr[i], minProd * arr[i]);
+//             maxProd = Math.max(arr[i], maxProd * arr[i]);
+//             minProd = Math.min(arr[i], minProd * arr[i]);
 
             
-            ans = Math.max(ans, maxProd);
-        }
+//             ans = Math.max(ans, maxProd);
+//         }
 
+//         return ans;
+//     }
+// }
+
+// class Solution {
+//     int maxProduct(int[] arr) {
+//         int ans = arr[0];
+//         for(int i=0; i<arr.length; i++){
+//             int prod = 1;
+//             for(int j=i; j<arr.length; j++){
+//                 prod = prod*arr[j];
+                
+//                 ans = Math.max(prod,ans);
+//             }
+//         }
+        
+//         return ans;
+//     }
+// }
+class Solution {
+    int maxProduct(int[] arr) {
+        int ans = arr[0];
+        int maxprod = arr[0];
+        int minprod = arr[0];
+        for(int i=1; i<arr.length; i++){
+           if(arr[i]<0){
+               int temp = maxprod;
+               maxprod = minprod;
+               minprod = temp;
+           }
+           
+           maxprod = Math.max(arr[i], maxprod*arr[i]);
+           minprod = Math.min(arr[i], minprod*arr[i]);
+           
+           ans = Math.max(ans, maxprod);
+        }
+        
         return ans;
     }
 }
+
+
