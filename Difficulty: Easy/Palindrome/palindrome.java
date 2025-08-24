ca707c3f4c2e86d39d613bf9ac8c782b8219ac6a
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-08-24 20:02:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome/palindrome.java
+++ Difficulty: Easy/Palindrome/palindrome.java
@@ -1,27 +1,13 @@
-// class Solution {
-//     public boolean isPalindrome(int n) {
-//         // code here
-//         String str = String.valueOf(n);
-//         String rev = new StringBuilder(str).reverse().toString();
-//         if(str.equals(rev)){
-//             return true;
-//         }
-//       return false;
-//     }
-// }
 class Solution {
     public boolean isPalindrome(int n) {
         // code here
-      if(n<0 || (n%10 == 0 && n!=0)){
-          return false;
-      }
-      int reversehalf =0;
-      
-      while(n> reversehalf){
-          reversehalf = reversehalf *10 + n%10;
-          n /=10;
-      }
-      
-      return (n==reversehalf) || n==reversehalf/10;
+        String str = String.valueOf(n);
+        String rev = new StringBuilder(str).reverse().toString();
+        
+        if(str.equals(rev)){
+            return true;
+        }
+        
+        return false;
     }
 }
