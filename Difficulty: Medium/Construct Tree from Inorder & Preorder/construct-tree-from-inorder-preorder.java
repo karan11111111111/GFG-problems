fec40490765dab4a405181564d861bb4fd79c93c
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-09-16 16:57:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Construct Tree from Inorder & Preorder/construct-tree-from-inorder-preorder.java
+++ Difficulty: Medium/Construct Tree from Inorder & Preorder/construct-tree-from-inorder-preorder.java
@@ -9,37 +9,43 @@ class Node {
     }
 }
 */
+
 class Solution {
     static int preIndex = 0;
 
     public static Node buildTree(int inorder[], int preorder[]) {
-        preIndex = 0;  // reset index if multiple test cases
-        return buildTreeHelper(inorder, preorder, 0, inorder.length - 1);
+        preIndex = 0; 
+        return build(inorder, preorder, 0, inorder.length - 1);
     }
 
-    private static Node buildTreeHelper(int inorder[], int preorder[], int inStart, int inEnd) {
-        if (inStart > inEnd) return null;
+    private static Node build(int[] inorder, int[] preorder, int inStart, int inEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
 
-        // Create the root node from preorder
+       
         Node root = new Node(preorder[preIndex++]);
 
-        // If there's only one element, it's a leaf node
-        if (inStart == inEnd) return root;
+      
+        if (inStart == inEnd) {
+            return root;
+        }
 
-        // Find the index of this root in inorder
+        
         int inIndex = search(inorder, inStart, inEnd, root.data);
 
-        // Recursively build left and right subtree
-        root.left = buildTreeHelper(inorder, preorder, inStart, inIndex - 1);
-        root.right = buildTreeHelper(inorder, preorder, inIndex + 1, inEnd);
+      
+        root.left = build(inorder, preorder, inStart, inIndex - 1);
+        root.right = build(inorder, preorder, inIndex + 1, inEnd);
 
         return root;
     }
 
-    private static int search(int[] inorder, int start, int end, int value) {
+   
+    private static int search(int[] inorder, int start, int end, int val) {
         for (int i = start; i <= end; i++) {
-            if (inorder[i] == value) return i;
+            if (inorder[i] == val) return i;
         }
-        return -1;  // should never happen
+        return -1;
     }
 }
