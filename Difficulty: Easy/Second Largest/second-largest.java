a70d7d337518bd17f5536c3f7304e4339fbedd7b
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-12-01 12:44:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,18 +1,23 @@
 class Solution {
     public int getSecondLargest(int[] arr) {
         // code here
-        TreeSet<Integer> set = new TreeSet <>();
-        
-        for(int a: arr){
-            set.add(a);
+        Set<Integer> st = new HashSet<>();
+        int secondlargest = Integer.MIN_VALUE;
+        int largest = Integer.MIN_VALUE;
+        for(int i =0; i<arr.length; i++){
+            st.add(arr[i]);
+        }
+      
+        for(int a : st){
+            if(a > largest ){
+                secondlargest = largest;
+                largest = a;
+            }else if(a> secondlargest){
+                secondlargest = a;
+            }
         }
         
-        if(set.size() <2){
-            return -1;
-        }
-        
-        set.pollLast();
-        
-        return set.last();
+        return secondlargest;
     }
+    
 }
