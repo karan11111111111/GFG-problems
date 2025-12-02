460a8695b322b4e9802580fd927d2ee2df15901d
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-12-02 12:33:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -0,0 +1,30 @@
+// User function Template for Java
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        
+        int n = arr.length;
+        
+        d=d%n;
+        
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+    }
+    
+    static void reverse(int arr[], int i, int n){
+        
+       
+        while(i<n){
+            
+            int temp = arr[i];
+            arr[i] = arr[n];
+            arr[n] = temp;
+            
+            i++;
+            n--;
+        }
+    }
+}
