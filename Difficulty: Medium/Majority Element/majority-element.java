eb932c71f79fecd5eca64aee8ee6e0bbe9310063
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-12-05 19:35:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -1,26 +1,52 @@
+// class Solution {
+//     int majorityElement(int arr[]) {
+//         // code here
+//         int count =0;
+//         int candidate=0;
+//         for(int i=0; i<arr.length; i++){
+//             if(count  ==0){
+//                 candidate = arr[i];
+//                 count =1;
+//             }else if(arr[i] == candidate){
+//                 count++;
+//             }else{
+//                 count --;
+//             }
+//         }
+//         int fre =0;
+//         for(int a : arr){
+//             if(a == candidate){
+//                 fre++;
+//             }
+//         }
+       
+//       return (fre>arr.length/2)? candidate : -1;
+        
+//     }
+// }
 class Solution {
     int majorityElement(int arr[]) {
         // code here
-        int count =0;
-        int candidate=0;
-        for(int i=0; i<arr.length; i++){
-            if(count  ==0){
-                candidate = arr[i];
-                count =1;
-            }else if(arr[i] == candidate){
-                count++;
-            }else{
-                count --;
-            }
-        }
-        int fre =0;
-        for(int a : arr){
-            if(a == candidate){
-                fre++;
-            }
-        }
-       
-       return (fre>arr.length/2)? candidate : -1;
+        int count =0; 
+        int candidate =0;
+       for(int i=0; i<arr.length; i++){
+           if(count ==0){
+               candidate = arr[i];
+               count =1;
+           }else if(candidate == arr[i]){
+               count++;
+           }else{
+               count--;
+           }
+       }
+       int frq=0;
+       for(int a: arr){
+           if(a == candidate){
+               frq++;
+           }
+       }
+        
+    return (frq> arr.length/2 ? candidate :-1 );
         
     }
 }
