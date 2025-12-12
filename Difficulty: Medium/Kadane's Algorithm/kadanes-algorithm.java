d3632bd655fe585fd2ef4d73f94415a297fd5cdd
Karan <144606876+karan11111111111@users.noreply.github.com>
2025-12-12 19:30:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -1,62 +1,24 @@
-// class Solution {
-//     int maxSubarraySum(int[] arr) {
-//         // Code here
-        
-//         int max = Integer.MIN_VALUE;
-        
-//         for(int i=0; i<arr.length; i++){
-//             int sum =0;
-//             for(int j=i; j<arr.length; j++){
-                
-//                 sum = sum+arr[j];
-//                 max = Math.max(max, sum);
-//             }
-//         }
-        
-//         return max;
-//     }
-// }
-// class Solution {
-//     int maxSubarraySum(int[] arr) {
-//         // Code here
-        
-//         int maxsum = Integer.MIN_VALUE;
-//         int sum =0;
-        
-//         for(int i=0; i<arr.length; i++){
-//           sum = arr[i] + sum;
-           
-//           if(sum >= maxsum){
-//               maxsum = sum;
-//           }
-//           if(sum < 0){
-//               sum =0;
-//           }
-           
-           
-//         }
-        
-//         return maxsum;
-//     }
-// }
 class Solution {
     int maxSubarraySum(int[] arr) {
         // Code here
         
-        int sum =0;
-        int maxsum = Integer.MIN_VALUE;
-        for(int i=0; i<arr.length; i++){
-            sum = sum+arr[i];
+       int sum = 0;
+       int maxsum = Integer.MIN_VALUE; // -infy
+       
+                            // 2, 3, -8, 7, -1, 2, 3
+       
+       for(int i=0; i<arr.length; i++){ // i=6
+           sum = sum+arr[i];  //  sum =  8 + 3 = 11
+           
+           if(sum > maxsum){
+               maxsum = sum;  // maxsum = 11
+           }
             
-            if(sum > maxsum){
-                maxsum = sum;
-            }
-            
-            if(sum < 0){
-                sum =0;
-            }
-        }
+           if(sum <0){ // -3
+               sum =0; // sum =0;
+           }
+       }
         
-        return maxsum;
+      return maxsum;
     }
 }
